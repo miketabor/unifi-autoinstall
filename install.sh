48dc11c70ab36577c39e83c9df0fc192458d2e04
Michael Tabor <mike@miketabor.com>
2018-11-01 15:59:20 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -28,6 +28,7 @@ echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | sudo t
 echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | sudo tee -a /etc/apt/sources.list.d/webupd8team-java.list
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886 
 sudo apt-get update 
+echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | debconf-set-selections || abort
 apt-get install oracle-java8-installer -y; apt-get install oracle-java8-set-default -y
 
 # Add Ubiquiti UniFi repo to system source list.
@@ -43,15 +44,15 @@ sudo apt-get update && sudo apt-get install unifi -y
 sudo apt-get install fail2ban -y
 
 # Copy config Fail2ban config files to preserve overwriting changes during Fail2ban upgrades.
-# sudo cp /etc/fail2ban/fail2ban.conf /etc/fail2ban/fail2ban.local
-# sudo cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
+sudo cp /etc/fail2ban/fail2ban.conf /etc/fail2ban/fail2ban.local
+sudo cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
 
 # Create ubiquiti Fail2ban definition and set fail regex. 
-# sudo echo -e '# Fail2Ban filter for Ubiquiti UniFi\n#\n#\n\n[Definition]\nfailregex =^.*Failed .* login .* <HOST>*\s*$
-# ' | sudo tee -a /etc/fail2ban/filter.d/ubiquiti.conf
+sudo echo -e '# Fail2Ban filter for Ubiquiti UniFi\n#\n#\n\n[Definition]\nfailregex =^.*Failed .* login .* <HOST>*\s*$
+' | sudo tee -a /etc/fail2ban/filter.d/ubiquiti.conf
 
 # Add ubiquiti JAIL to Fail2ban setting log path and blocking IPs after 3 failed logins within 15 minutes for 1 hour.
-# sudo echo -e '\n[ubiquiti]\nenabled  = true\nfilter   = ubiquiti\nlogpath  = /usr/lib/unifi/logs/server.log\nmaxretry = 3\nbantime = 3600\nfindtime = 900\nport = 8443\nbanaction = iptables[name=ubiquiti, port=8443, protocol=tcp]' | sudo tee -a /etc/fail2ban/jail.local
+sudo echo -e '\n[ubiquiti]\nenabled  = true\nfilter   = ubiquiti\nlogpath  = /usr/lib/unifi/logs/server.log\nmaxretry = 3\nbantime = 3600\nfindtime = 900\nport = 8443\nbanaction = iptables[name=ubiquiti, port=8443, protocol=tcp]' | sudo tee -a /etc/fail2ban/jail.local
 
 # Restart Fail2ban to apply changes above.
 sudo service fail2ban restart
